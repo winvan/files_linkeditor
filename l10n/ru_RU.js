59d7f770e2de8660cb0e36eee877b2ef14b4ee24
winvan <45930893+winvan@users.noreply.github.com>
2019-02-25 09:01:48 +0700

    Create ru_RU.js

---
+++ l10n/ru_RU.js
@@ -0,0 +1,20 @@
+OC.L10N.register(
+	'files_linkeditor',
+	{
+		'You are about to visit:': 'Адрес, который вы собираетесь посетить:',
+		'Saving failed!': 'Ошибка сохранения',
+		'Edit link': 'Редактировать ссылку',
+		'View link': 'Просмотр ссылки',
+		'This link-file doesn\'t seem to be valid. – You can fix this by editing the file.': 'Файл ссылки, возможно поврежден, вы можетет его отредактировать',
+		'A slight problem': 'Небольшие проблемы',
+		'Cancel': 'Отмена',
+		'Visit link': 'Посетить ссылку',
+		'An error occurred!': 'Произошла ошибка!',
+		'Link target URL': 'Целевой URL ссылки',
+		'e.g. https://example.org': 'например https://example.org',
+		'Save': 'Сохранить',
+		'New link': 'Новая ссылка',
+		'Link.URL': 'Link.URL'
+	},
+'nplurals=2; plural=(n != 1);');
+
